704977a1765182f35423b05f335f8f360062b52d
cindy <1177031+cindywu@users.noreply.github.com>
2023-01-09 20:03:22 -1000

    display items in /uses

--- src/frontend/uses.tsx
+++ src/frontend/uses.tsx
@@ -1,11 +1,30 @@
 import React from 'react'
 import styles from './uses.module.css'
+import { randomItem } from "../datamodel/item";
+import { useItemIDs } from "../datamodel/subscriptions";
 
-export default function Uses() {
+type UsesProps = {
+  reflect: any
+}
+
+export default function Uses({ reflect } : UsesProps) {
+
+  const itemIDs = useItemIDs(reflect);
+  console.log('itemIDs', itemIDs)
   return (
     <div className={styles.container}>
       <div className={styles.header}>Uses</div>
       <div className={styles.subHeader}>Objects have memories</div>
+      <div className={styles.itemsList}>
+        {itemIDs && itemIDs.map((id) => {
+          return (
+            <div key={id}>
+              {id}
+            </div>
+          )
+        })
+        }
+      </div>
     </div>
   )
 }
--- src/pages/uses/index.tsx
+++ src/pages/uses/index.tsx
@@ -1,8 +1,76 @@
-import React from 'react'
+import { useEffect, useState } from "react";
+import { Reflect } from "@rocicorp/reflect";
+import { Designer } from "../../frontend/designer";
+import { Nav } from "../../frontend/nav";
+import { M, clientMutators } from "../../datamodel/mutators";
+import { randUserInfo } from "../../datamodel/client-state";
+import { nanoid } from "nanoid";
+import { consoleLogSink, OptionalLoggerImpl } from "@rocicorp/logger";
+import { DataDogBrowserLogSink } from "../../frontend/data-dog-browser-log-sink";
+import { workerWsURI } from "../../util/host";
 import Uses from '../../frontend/uses'
 
-export default function index() {
+export default function Home() {
+  const [reflect, setReflectClient] = useState<Reflect<M> | null>(null);
+  const [online, setOnline] = useState(false);
+
+  const logSink = process.env.NEXT_PUBLIC_DATADOG_CLIENT_TOKEN
+    ? new DataDogBrowserLogSink()
+    : consoleLogSink;
+  const logger = new OptionalLoggerImpl(logSink);
+
+  useEffect(() => {
+    const roomID = `KkdFw3`;
+
+    (async () => {
+      logger.info?.(`Connecting to worker at ${workerWsURI}`);
+      const userID = nanoid();
+      const r = new Reflect<M>({
+        socketOrigin: workerWsURI,
+        onOnlineChange: setOnline,
+        userID,
+        roomID,
+        auth: JSON.stringify({
+          userID,
+          roomID,
+        }),
+        logSinks: [logSink],
+        mutators: clientMutators,
+      });
+
+      const defaultUserInfo = randUserInfo();
+      await r.mutate.initClientState({
+        id: await r.clientID,
+        defaultUserInfo,
+      });
+      await r.mutate.initShapes();
+
+      setReflectClient(r);
+    })();
+  }, []);
+
+  if (!reflect) {
+    return null;
+  }
+
   return (
-    <Uses/>
-  )
+    // <div
+    //   style={{
+    //     position: "absolute",
+    //     display: "flex",
+    //     flexDirection: "column",
+    //     left: 0,
+    //     top: 0,
+    //     width: "100%",
+    //     height: "100%",
+    //     background: "rgb(229,229,229)",
+    //   }}
+    // >
+    //   <Nav reflect={reflect} online={online} />
+    //   <Designer reflect={reflect} logger={logger} />
+    // </div>
+    <Uses
+      reflect={reflect}
+    />
+  );
 }
